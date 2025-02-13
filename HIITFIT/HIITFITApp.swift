d2f099c1731ab54447760495c93d01ae00e6d98e
Joana2337 <joana.osei37@myhunter.cuny.edu>
2025-02-12 21:01:30 -0500

    Initial Commit

---
+++ HIITFIT/HIITFITApp.swift
@@ -0,0 +1,17 @@
+//
+//  HIITFITApp.swift
+//  HIITFIT
+//
+//  Created by Joanne on 2/12/25.
+//
+
+import SwiftUI
+
+@main
+struct HIITFITApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ HIITFITTests/HIITFITTests.swift
@@ -0,0 +1,16 @@
+//
+//  HIITFITTests.swift
+//  HIITFITTests
+//
+//  Created by Joanne on 2/12/25.
+//
+
+import Testing
+
+struct HIITFITTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
